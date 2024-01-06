372716c7d10eb100502611c7725778714190d27e
chavanjayesh9507 <155827550+chavanjayesh9507@users.noreply.github.com>
2024-01-06 15:33:40 +0000

    conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "23abc"
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+//console.log(typeof(valueInNumber))
+//console.log(valueInNumber)
+
+//   "23" => 23
+//  "23abc" => NaN
+//  true => 1; false => 0;
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(typeof booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+//  1 => true; 0 => false
+//  "" => false
+//  "jayesh" => true      
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
